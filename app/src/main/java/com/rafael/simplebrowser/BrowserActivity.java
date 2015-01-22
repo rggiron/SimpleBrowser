b2fcd0f91128351080661a9eac3f756cb1e6d988
rggiron <rggiron@gmail.com>
2015-01-22 21:56:33 +0100

    first step, OK

--- app/src/main/java/com/rafael/simplebrowser/BrowserActivity.java
+++ app/src/main/java/com/rafael/simplebrowser/BrowserActivity.java
@@ -2,26 +2,23 @@ package com.rafael.simplebrowser;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class BrowserActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_browser);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_browser, menu);
         return true;
     }
 
@@ -30,26 +27,13 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void bt_search_onclick(View v)
-    {
-        if (v.getId() == R.id.bt_search)
-        {
-            //1. Get the text
-            String searchText = ((EditText)findViewById(R.id.et_search_field)).getText().toString();
-
-            Log.d("MAIN_ONCLICK","texto a buscar:'" + searchText + "'");
-            //2. Launch browser activity passing the text
-
-        }
     }
 }
